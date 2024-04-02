701d51a6c4c5a8e58fd6ab2551aacfd33f2fb094
Roshan Kumar Sharma <rksharma77177@gmail.com>
2024-04-02 13:07:15 +0530

    changes

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, status, Depends
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
 from authentication import *
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm
+)
 from tortoise.signals import  post_save 
 from typing import List, Optional, Type
 from tortoise import BaseDBAsyncClient
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from emails import *
+import templates
+from pathlib import Path
+
 
 app = FastAPI()
+
+oath2_scheme = OAuth2PasswordBearer(tokenUrl = 'token')
+
+@app.post('/token')
+async def generate_token(request_form: OAuth2PasswordRequestForm = Depends()):
+    token = await token_generator(request_form.username, request_form.password)
+    return {'access_token' : token, 'token_type' : 'bearer'}
+
+async def get_current_user(token: str = Depends(oath2_scheme)):
+    try:
+        payload = jwt.decode(token, config_credentials['SECRET'], algorithms = ['HS256'])
+        user = await User.get(id = payload.get("id"))
+    except:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED, 
+            detail = "Invalid username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return await user
+
+@app.post("/user/me")
+async def user_login(user: user_pydanticIn = Depends(get_current_user)):
+    business = await Business.get(owner = user)
+    return {
+        "status": "ok",
+        "data": 
+        {
+            "username": user.username,
+            "email": user.email,
+            "verified": user.is_verified,
+            "joined date": user.join_date.strftime("%b %d %Y")
+        }
+    }
 
 @post_save(User)
 async def create_business(
@@ -20,9 +64,11 @@ async def create_business(
         business_obj = await Business.create(
                 business_name = instance.username, owner = instance)
         await business_pydantic.from_tortoise_orm(business_obj)
-        
+        # await send_email([instance.email], instance)
+
 @app.post('/registration')
 async def user_registration(user: user_pydanticIn):
+    # breakpoint()
     user_info = user.dict(exclude_unset = True)
     user_info['password'] = get_password_hash(user_info['password'])
     user_obj = await User.create(**user_info)
@@ -31,6 +77,22 @@ async def user_registration(user: user_pydanticIn):
     return {"status" : "ok", 
             "data" : 
                 f"Hello {new_user.username} thanks for choosing our services. Please check your email inbox and click on the link to confirm your registration."} 
+
+template_dir = "C:\\Users\\NH2882\\Desktop\\revision\\templates"
+templates = Jinja2Templates(directory=str(template_dir))
+@app.get('/verification', response_class=HTMLResponse)
+async def email_verification(request: Request, token: str):
+    user = await verify_token(token)
+    if user and not user.is_verified:
+        user.is_verified = True
+        await user.save()
+        return templates.TemplateResponse("verification.html", {"request": request, "username": user.username})
+
+    raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED, 
+            detail = "Invalid username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 register_tortoise(
     app,
