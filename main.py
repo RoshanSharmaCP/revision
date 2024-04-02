33e347cf943adb7e002ccfb0258d3b1a086a18a9
Roshan Kumar Sharma <rksharma77177@gmail.com>
2024-04-02 14:50:23 +0530

    image upload business and products

--- main.py
+++ main.py
@@ -15,10 +15,18 @@ from emails import *
 import templates
 from pathlib import Path
 
+from fastapi import File, UploadFile
+import secrets
+from fastapi.staticfiles import StaticFiles
+from PIL import Image
+
+
 
 app = FastAPI()
 
 oath2_scheme = OAuth2PasswordBearer(tokenUrl = 'token')
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.post('/token')
 async def generate_token(request_form: OAuth2PasswordRequestForm = Depends()):
@@ -93,6 +101,91 @@ async def email_verification(request: Request, token: str):
             detail = "Invalid username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+@app.post("/uploadfile/profile")
+async def create_upload_file(file: UploadFile = File(...), 
+                                user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = "./static/images/"
+    filename = file.filename
+    extension = filename.split(".")[1]
+
+    if extension not in ["jpg", "png"]:
+        return {"status" : "error", "detail" : "file extension not allowed"}
+
+    token_name = secrets.token_hex(10)+"."+extension
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+
+
+    # pillow
+    img = Image.open(generated_name)
+    img = img.resize(size = (200,200))
+    img.save(generated_name)
+
+    file.close()
+
+    business = await Business.get(owner = user)
+    owner = await business.owner
+
+    if owner == user:
+        business.logo = token_name
+        await business.save()
+    
+    else:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED, 
+            detail = "Not authenticated to perform this action",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    file_url = "localhost:8000" + generated_name[1:]
+    return {"status": "ok", "filename": file_url}
+
+@app.post("/uploadfile/product/{id}")
+async def create_upload_file(id: int, file: UploadFile = File(...), 
+                                user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = "./static/images/"
+    filename = file.filename
+    extension = filename.split(".")[1]
+
+    if extension not in ["jpg", "png"]:
+        return {"status" : "error", "detail" : "file extension not allowed"}
+
+    token_name = secrets.token_hex(10)+"."+extension
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+
+
+    # pillow
+    img = Image.open(generated_name)
+    img = img.resize(size = (200,200))
+    img.save(generated_name)
+
+    file.close()
+
+    product = await Product.get(id = id)
+    business = await product.business
+    owner = await business.owner
+
+    if owner == user:
+        product.product_image = token_name
+        await product.save()
+    
+    else:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED, 
+            detail = "Not authenticated to perform this action",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+    file_url = "localhost:8000" + generated_name[1:]
+    return {"status": "ok", "filename": file_url}
+    
 
 register_tortoise(
     app,
